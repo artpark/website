5202eea5c755ed4d0d19f0eaece659b660087b64
Art Parkeenvincha <thanut@satang.com>
2020-01-22 00:48:21 -0800

    Updates

---
+++ precache-manifest.8b30325f2db21c42a3378b971d5ff3de.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4af36b6b075d5baedd3bd7b2f2e4ae9a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f191ef1f47e120f4b03c",
+    "url": "/static/css/2.ff8bd605.chunk.css"
+  },
+  {
+    "revision": "59ce6836d3953698614c",
+    "url": "/static/css/main.01e516d1.chunk.css"
+  },
+  {
+    "revision": "f191ef1f47e120f4b03c",
+    "url": "/static/js/2.b16f866a.chunk.js"
+  },
+  {
+    "revision": "59ce6836d3953698614c",
+    "url": "/static/js/main.16d92ed3.chunk.js"
+  },
+  {
+    "revision": "36608967e06bd52c3273",
+    "url": "/static/js/runtime-main.47301523.js"
+  },
+  {
+    "revision": "dde59e8922528b1ec95694257435e4bf",
+    "url": "/static/media/resume.dde59e89.pdf"
+  },
+  {
+    "revision": "3cc3ebd5f050b2d8cb04d4594753667f",
+    "url": "/static/media/satangCropped.3cc3ebd5.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/~tparkeen/precache-manifest.e3e60bfb5f054a5459791772bdaf8d60.js"
+  "/precache-manifest.8b30325f2db21c42a3378b971d5ff3de.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/~tparkeen/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
