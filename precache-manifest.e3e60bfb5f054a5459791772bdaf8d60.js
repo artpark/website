dc054ae174377a5ce19280147a0a08fa67b2687d
Art Parkeenvincha <thanut@satang.com>
2020-01-22 00:43:05 -0800

    Updates

---
+++ precache-manifest.e3e60bfb5f054a5459791772bdaf8d60.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea63cebd2129176a5abbdfbe91c01af2",
+    "url": "/~tparkeen/index.html"
+  },
+  {
+    "revision": "c12e5e6c9091efdbd380",
+    "url": "/~tparkeen/static/css/2.ff8bd605.chunk.css"
+  },
+  {
+    "revision": "590ee05bfac411193f49",
+    "url": "/~tparkeen/static/css/main.7b3c3b4e.chunk.css"
+  },
+  {
+    "revision": "c12e5e6c9091efdbd380",
+    "url": "/~tparkeen/static/js/2.b1ecd181.chunk.js"
+  },
+  {
+    "revision": "590ee05bfac411193f49",
+    "url": "/~tparkeen/static/js/main.f57cd129.chunk.js"
+  },
+  {
+    "revision": "0e74715d793788da29b7",
+    "url": "/~tparkeen/static/js/runtime-main.71744c36.js"
+  },
+  {
+    "revision": "dde59e8922528b1ec95694257435e4bf",
+    "url": "/~tparkeen/static/media/resume.dde59e89.pdf"
+  },
+  {
+    "revision": "3cc3ebd5f050b2d8cb04d4594753667f",
+    "url": "/~tparkeen/static/media/satangCropped.3cc3ebd5.jpg"
+  }
+]);
