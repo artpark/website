24e249230a07dcb9bfea6c14aa5c7cf269bffff1
Art Parkeenvincha <thanut@satang.com>
2020-01-24 00:16:25 -0800

    projects page almost done

--- src/pages/AboutPage/AboutPage.js
+++ src/pages/AboutPage/AboutPage.js
@@ -1,9 +1,23 @@
 import React from 'react'
-import { Layout } from 'antd'
+import { Tooltip, Timeline, Icon } from 'antd'
+
+// Look into ant design anchor
 
 const AboutPage = () => {
   return (
     <div className="about-page-container">
+      {/* <Timeline mode="alternate" pending pendingDot={<Icon type="question" />}>
+        <Timeline.Item color="#ff5e6c">Was born</Timeline.Item>
+        <Timeline.Item color="#ff5e6c">
+          Lived in <Tooltip title="prompt text">Thailand</Tooltip>
+        </Timeline.Item>
+        <Timeline.Item color="#ff5e6c">Moved to Canada</Timeline.Item>
+        <Timeline.Item color="#ff5e6c">Moved to San Francisco</Timeline.Item>
+        <Timeline.Item color="#ff5e6c">Moved to Santa Cruz</Timeline.Item>
+        <Timeline.Item color="#ff5e6c">
+          Univeristy of California, Santa Cruz
+        </Timeline.Item>
+      </Timeline> */}
       <h1>Coming soon!</h1>
     </div>
   )
--- src/pages/ProjectsPage/ProjectsPage.js
+++ src/pages/ProjectsPage/ProjectsPage.js
@@ -1,34 +1,62 @@
 import React from 'react'
 import { Card, Col, Row } from 'antd'
 
+import cleanthisspace from '../../assets/images/cleanthisspace.png'
+import acwww from '../../assets/images/ACWWW.png'
+import database from '../../assets/images/database.png'
+
+const { Meta } = Card
+
 const ProjectsPage = () => {
+  const cardCover = img => (
+    <div
+      className="card-cover"
+      style={{ backgroundImage: `url(${img})` }}
+    ></div>
+  )
+
+  const projectCard = ({ coverImg, title, description, link }) => {
+    return (
+      <Card
+        className="projects-card"
+        bordered={false}
+        hoverable
+        cover={cardCover(coverImg)}
+      >
+        <Meta title={title} description={description} />
+      </Card>
+    )
+  }
+
   return (
     <div className="projects-container">
-      {/* <Row gutter={[25, 25]}>
-        <Col span={12}>
-          <Card className="projects-card" title="Card title" bordered={false}>
-            Card content
-          </Card>
+      <Row gutter={[25, 25]}>
+        <Col lg={8} md={24}>
+          {projectCard({
+            coverImg: cleanthisspace,
+            title: 'hi',
+            description: 'hi',
+            link: 'hi',
+          })}
         </Col>
-        <Col span={12}>
-          <Card className="projects-card" title="Card title" bordered={false}>
-            Card content
-          </Card>
+        <Col lg={8} md={24}>
+          {projectCard({
+            coverImg: acwww,
+            title: 'hi',
+            description: 'hi',
+            link: 'hi',
+          })}
+        </Col>
+        <Col lg={8} md={24}>
+          {projectCard({
+            coverImg: database,
+            title: 'hi',
+            description: 'hi',
+            link: 'hi',
+          })}
         </Col>
       </Row>
-      <Row gutter={[25, 25]}>
-        <Col span={12}>
-          <Card className="projects-card" title="Card title" bordered={false}>
-            Card content
-          </Card>
-        </Col>
-        <Col span={12}>
-          <Card className="projects-card" title="Card title" bordered={false}>
-            Card content
-          </Card>
-        </Col>
-      </Row> */}
-      <h1>Coming soon!</h1>
+      {/* <h1>Coming soon!</h1> */}
     </div>
   )
 }
