268d969f368c95b1a8beb1281d8d8ef881ebfa35
Art <art.parkeenvincha@gmail.com>
2022-05-13 21:17:12 -0500

    Updated project description

--- src/pages/ProjectsPage/ProjectsPage.js
+++ src/pages/ProjectsPage/ProjectsPage.js
@@ -8,7 +8,7 @@ import database from '../../assets/images/database.png'
 const { Meta } = Card
 
 const ProjectsPage = () => {
-  const cardCover = img => (
+  const cardCover = (img) => (
     <div
       className="card-cover"
       style={{ backgroundImage: `url(${img})` }}
@@ -22,6 +22,7 @@ const ProjectsPage = () => {
         bordered={false}
         hoverable
         cover={cardCover(coverImg)}
+        onClick={() => (link ? window.open(link, '_blank') : null)}
       >
         <Meta title={title} description={description} />
       </Card>
@@ -34,29 +35,30 @@ const ProjectsPage = () => {
         <Col lg={8} md={24}>
           {projectCard({
             coverImg: cleanthisspace,
-            title: 'hi',
-            description: 'hi',
-            link: 'hi',
+            title: 'Clean This Space!',
+            description: `A crowd-source platform for enabling environmental activism. I worked as the lead front-end developer for the team. I utilized React.js, SCSS, and various Google APIs.
+              Our team was the third place winner of the Earth Hacks category of CruzHacks 2020.`,
+            link: 'https://devpost.com/software/temp-2k3y9e',
           })}
         </Col>
         <Col lg={8} md={24}>
           {projectCard({
             coverImg: acwww,
-            title: 'hi',
-            description: 'hi',
-            link: 'hi',
+            title: 'Animal Crossing in WebGL',
+            description:
+              'Recreating the Nintendo game "Animal Crossing" in WebGL. First place winner as part of a class contest in the computer graphics class (CMPS 160).',
+            link: 'https://github.com/artpark/AnimalCrossingWebGL',
           })}
         </Col>
         <Col lg={8} md={24}>
           {projectCard({
             coverImg: database,
-            title: 'hi',
-            description: 'hi',
-            link: 'hi',
+            title: 'Distributed Key-Value Storage System',
+            description:
+              'A fault tolerant causal and eventually consistent key-value storage system, similar to Redis. Created for the class "distributed systems" (CMPS 128). Constructed using Python, Flask, and Docker.',
           })}
         </Col>
       </Row>
-      {/* <h1>Coming soon!</h1> */}
     </div>
   )
 }
